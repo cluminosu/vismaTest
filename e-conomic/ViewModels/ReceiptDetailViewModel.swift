ad7bf0f5937466dbb46d215d4c7ab74132c039ce
Claudiu Luminosu <claudiu.luminosu@mailbox.org>
2024-07-23 16:33:34 +0300

    initial outline

---
+++ e-conomic/ViewModels/ReceiptDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ReceiptDetailViewModel.swift
+//  e-conomic
+//
+//  Created by Claudiu Luminosu on 23.07.2024.
+//
+
+import UIKit
+
+class ReceiptDetailViewModel: NSObject {
+
+}
---
+++ e-conomic/ViewModels/ReceiptsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ReceiptsViewModel.swift
+//  e-conomic
+//
+//  Created by Claudiu Luminosu on 23.07.2024.
+//
+
+import UIKit
+
+class ReceiptsViewModel: NSObject {
+
+}
