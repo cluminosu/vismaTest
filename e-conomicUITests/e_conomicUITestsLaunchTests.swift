c6861bc025ab7c92570a6c76b85fd4966719ae8c
Claudiu Luminosu <claudiu.luminosu@mailbox.org>
2024-07-23 16:12:07 +0300

    Initial Commit

---
+++ e-conomicUITests/e_conomicUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  e_conomicUITestsLaunchTests.swift
+//  e-conomicUITests
+//
+//  Created by Claudiu Luminosu on 23.07.2024.
+//
+
+import XCTest
+
+final class e_conomicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
